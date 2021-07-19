ad7b9778c343cbcf80bc1cd47104549623db535e
Julie Ha <julieha124@gmail.com>
2021-07-19 01:16:29 -0400

    changes

--- bloom.py
+++ bloom.py
@@ -6,7 +6,7 @@ from LSH import LSH
 import pickle
 
 class bftree(object):
-    def __init__(self, branching_f, error_rate, max_elements, n=1024, r=307, c=0.5 * 1024/307, s=12, l=1000):
+    def __init__(self, branching_f, error_rate, max_elements, n=1024, r=307, c=0, s=12, l=1000):
         self.branching_factor = branching_f
         self.error_rate = error_rate
         self.max_elem = max_elements
@@ -15,26 +15,26 @@ class bftree(object):
         self.eLSH = None
         self.n = n
         self.r = r
-        self.c = c
+        self.c = 0.5 * n/r
         self.s = s
         self.l = l 
 
     #creates a new bloom filter with elements from actual_elements
     def new_filter(self, num_expected_elements, actual_elements=None): 
-        temp = BloomFilter(max_elements=self.max_elem, error_rate=self.error_rate)
+        temp = BloomFilter(max_elements=(self.l*num_expected_elements), error_rate=self.error_rate)
         return temp
 
-    def add_with_eLSH(self, filter, eLSH, elements): #add to bloom filter with applying eLSH 
+    def add_with_eLSH(self, filter, elsh, elements): #add to bloom filter with applying eLSH 
         for i in elements: #convert i to string first? 
             # print(i)
-            if type(i) == str: 
-                temp = ''.join(format(ord(k), '08b') for k in i)
-                i = [int(k) for k in temp]
-                i = i + ([0 for i in range(self.n - len(i))]) #padding
-            current_hash = eLSH.hash(i)
+            # if type(i) == str: 
+            #     temp = ''.join(format(ord(k), '08b') for k in i)
+            #     i = [int(k) for k in temp]
+            #     i = i + ([0 for i in range(self.n - len(i))]) #padding
+            current_hash = elsh.hash(i)
             for j in current_hash: 
-                # print(j)
-                p = pickle.dumps(j)
+                # p = pickle.dumps(j)
+                p = str(j)
                 filter.add(p) #might have to convert this to string first 
 
         return filter        
@@ -49,12 +49,11 @@ class bftree(object):
         self.eLSH = [None for i in range(tree_depth + 1)]
 
         #elsh object for root 
-        current_elsh = extended_lsh.eLSH(LSH, self.n, self.r, self.c, self.s, num_elements)
+        current_elsh = extended_lsh.eLSH(LSH, self.n, self.r, self.c, self.s, self.l)
         self.eLSH[level] = current_elsh
         bfilter_root = self.new_filter(num_elements)
-        self.add_with_eLSH(bfilter_root, current_elsh, elements)
-        self.tree.create_node("root", self.root, data=bfilter_root)
-
+        f = self.add_with_eLSH(bfilter_root, current_elsh, elements)
+        self.tree.create_node("root", self.root, data=f)
         
         # print("tree depth", tree_depth)
         current_node = self.root
@@ -63,7 +62,7 @@ class bftree(object):
             level += 1
             nodes_in_level = self.branching_factor**level
             items_in_filter = self.branching_factor**(tree_depth-level)
-            current_elsh = extended_lsh.eLSH(LSH, self.n, self.r, self.c, self.s, nodes_in_level)
+            current_elsh = extended_lsh.eLSH(LSH, self.n, self.r, self.c, self.s, self.l) #new elsh for new tree depth 
             self.eLSH[level] = current_elsh
             for n in range(nodes_in_level): 
                 current_node += 1
@@ -75,59 +74,67 @@ class bftree(object):
                     self.tree.create_node(str(current_node), current_node, data=None, parent=parent_node)
                 else: 
                     bf = self.new_filter(items_in_filter)
-                    self.add_with_eLSH(bf, current_elsh, elements_in_filter)
-                    self.tree.create_node(str(current_node), current_node, data=bf, parent=parent_node)
+                    f = self.add_with_eLSH(bf, current_elsh, elements_in_filter)
+                    self.tree.create_node(str(current_node), current_node, data=f, parent=parent_node)
 
         return self.tree
 
-    def search(self, list_item): #list_item : if any item in the list_item is in bloom filter visit all children \\ kinda funky, but list_item must be list 
+    def search(self, item): #list_item : if any item in the list_item is in bloom filter visit all children \\ kinda funky, but list_item must be list 
         depth = self.tree.depth()
         stack = [] 
         nodes_visited = [] 
         leaf_nodes = [] 
-        access_depth = [[] for i in range(depth+1)] #nodes accessed per depth s
+        access_depth = [[] for i in range(depth+1)] #nodes accessed per depth 
         root_bf = self.tree[self.root].data
 
-        for i in range(len(list_item)):
-            thing = list_item[i]
-            if type(thing) == str: 
-                temp = ''.join(format(ord(k), '08b') for k in thing)
-                thing = [int(k) for k in temp]
-                thing = thing + ([0 for i in range(self.n - len(list_item))]) #padding
-                list_item[i] = thing
+        # for i in range(len(list_item)):
+        #     thing = list_item[i]
+        #     if type(thing) == str: 
+        #         temp = ''.join(format(ord(k), '08b') for k in thing)
+        #         thing = [int(k) for k in temp]
+        #         thing = thing + ([0 for i in range(self.n - len(list_item))]) #padding
+        #         list_item[i] = thing
             
 
         access_depth[0].append(self.root)
         current_elsh = self.eLSH[0]
-        for item in list_item: 
+        # for item in list_item: 
             # print(item)
-            current_hash = current_elsh.hash(item)
-            # print(current_hash)
-            for j in current_hash:
-                p = pickle.dumps(j)
-                if p in root_bf: 
-                    stack.append(self.root)
-                    break
+        current_hash = current_elsh.hash(item)
+        # print(current_hash)
+        for j in current_hash:
+            # p = pickle.dumps(j)
+            p = str(j)
+            if p in root_bf: 
+                stack.append(self.root)
+                break
 
         while stack != []: 
             # print("stack", stack)
             current_node = stack.pop()
             nodes_visited.append(current_node)
             children = self.tree.children(current_node) 
+            
             if children != []: 
+                child_depth = self.tree.depth(current_node) + 1
+                current_elsh = self.eLSH[child_depth]
+
+                current_hash = current_elsh.hash(item)
+
                 for c in children: 
-                    child = c.identifier
-                    child_depth = self.tree.depth(child)
-                    current_elsh = self.eLSH[child_depth]
-                    access_depth[child_depth].append(child)
+                    child = c.identifier 
+                    access_depth[child_depth].append(child)                    
                     if self.tree[child].data != None: 
-                        for i in list_item:
-                            current_hash = current_elsh.hash(i)
-                            for j in current_hash:
-                                p = pickle.dumps(j)
-                                if p in self.tree[child].data: 
-                                    stack.append(child)
-                                    break
+                        count = 0 
+                        for j in current_hash: 
+                            count += 1
+                            # p = pickle.dumps(j)
+                            p = str(j) 
+                            if p in self.tree[child].data: 
+                                print("CHILD:", child)
+                                print("COUNT HASH:", count)
+                                stack.append(child)
+                                break
             else: 
                 leaf_nodes.append(current_node)
         return nodes_visited, leaf_nodes, access_depth
@@ -169,12 +176,12 @@ def find_size(bftree):
 from other import try_data
 import random
 
-t = bftree(2, 0.01, 8)
+t = bftree(2, 0.01, 9)
 t.build_index(try_data)
 t.tree.show()
 attempt = try_data[0]
-for i in range(100): 
-    index = random.randrange(0,1024) 
-    attempt[index] = ~attempt[index]
-s = t.search([attempt])
+# for i in range(200): 
+#     index = random.randrange(0,1024) 
+#     attempt[index] = not attempt[index]
+s = t.search(attempt)
 print(s)
