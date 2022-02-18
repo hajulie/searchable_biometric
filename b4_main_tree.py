465f401c322e1bb8bfa8e796550214c04cded9a3
Julie Ha <julieha124@gmail.com>
2022-02-18 11:41:17 -0500

    commit implementation w subtrees

---
+++ b4_main_tree.py
@@ -0,0 +1,142 @@
+#bloom4, each tree is a lsh 
+
+from bloom_filter2 import BloomFilter
+from treelib import Node, Tree
+import math, os, sys
+import eLSH as eLSH_import
+from LSH import LSH
+import pickle
+from Crypto.Util.Padding import pad, unpad
+
+import pyoram
+from pyoram.util.misc import MemorySize
+from pyoram.oblivious_storage.tree.path_oram import PathORAM
+
+from b4_node import node
+from b4_subtree import subtree
+
+storage_name = "heap.bin"
+
+class main_tree(object): 
+
+    def __init__(self, branching_factor, error_rate, n=1024, r=307, c=0.5*(1024/307), s=12, l=1000): 
+        self.branching_factor = branching_factor
+        self.error_rate = error_rate
+        self.max_elem = None
+
+        self.depth = None
+        self.root = branching_factor - 1
+
+        self.eLSH = None
+        self.n = n
+        self.r = r
+        self.c = c
+        self.s = s #samples s bits l times 
+        self.l = l
+        self.lsh = None
+        self.hash = [ [] for i in range(l) ] #lsh output 
+
+        self.subtrees = [None for i in range(l)] #keeps track of subtrees 
+        self.hash_to_iris = {}
+
+    #compute eLSH and returns the list of length l
+    def compute_eLSH_one(self, element): 
+        output = self.eLSH.hash(element) #length of l 
+
+        for index, h in enumerate(output):
+            self.hash[index] += [h] 
+            if str(h) in self.hash_to_iris:
+                self.hash_to_iris[str(h)] += [element]
+            else: 
+                self.hash_to_iris[str(h)] = [element]
+        return output
+
+    def compute_eLSH(self, elements): #computes the mapping from hash to iris 
+        for i in elements: 
+            self.compute_eLSH_one(i)
+
+    #creates a new node: bloom filter with elements from actual_elements
+    def new_node(self, num_expected_elements, actual_elements=None): 
+        bf = BloomFilter(max_elements=(self.l*num_expected_elements), error_rate=self.error_rate)
+        n = node(bf)
+        return n
+
+    #TREE SPECIFIC FUNCS 
+
+    def build_index(self, elements):
+        num_elements = len(elements)
+        level = 0 #levels increase going down 
+
+        self.eLSH = eLSH_import.eLSH(LSH, self.n, self.r, self.c, self.s, self.l)
+        self.lsh = self.eLSH.hashes
+        print("self.lsh:", len(self.lsh))
+        self.compute_eLSH(elements)
+
+        for h in range(self.l): 
+            st = subtree(self.branching_factor, self.error_rate, self.lsh[h])
+            st.build_tree( elements )
+            self.subtrees[h] = st 
+
+        print(self.subtrees) #print subtrees
+        for i in self.subtrees: 
+            print("subtree:", i)
+            print("show", i.tree)
+
+    def search(self, item): 
+        # print("tree, searching for:", item)
+        nodes_visited = [] 
+        leaf_nodes = [] 
+        returned_iris = [] 
+        access_depth = [] #nodes accessed per depth 
+        
+        for sub_tree in self.subtrees: 
+            st_nodes, st_leaf, st_access = sub_tree.search(item)
+
+            # for leaf in st_leaf:
+            #     iris = self.hash_to_iris[sub_tree.get_leaf_node(leaf).data[0]]
+            #     returned_iris.append(iris)
+
+            nodes_visited += st_nodes
+            leaf_nodes += st_leaf
+            access_depth += access_depth
+
+        return nodes_visited, leaf_nodes, returned_iris, access_depth
+
+if __name__ == '__main__':
+    #small test 
+    import random
+
+    n = 2
+    fpr = 0.0001 
+    temp_l = 1
+
+    try_nums = [2]
+
+    for n in try_nums: 
+        print('\n--- size of database = %i ---' %n )
+        try_data = ([[random.getrandbits(1) for i in range(1024)] for i in range(n)])
+
+        t = main_tree(2, fpr, l = temp_l)
+        t.build_index(try_data)
+        # t.tree.show()
+        child = random.randint(0,n-1)
+        attempt = try_data[child]
+        p_child = child
+        # print("Depth of tree: ", t.tree.depth())
+        print("Search for leaf %i" % p_child)
+        s = t.search(attempt)
+        print("All nodes visited:", s[0])
+        print("Matched leaf nodes:", s[1])
+        print("Nodes matched at each level:", s[2])
+        
+        match = 0 
+        non = 0
+        for i in s[3]: 
+            for r in i: 
+                if r == attempt: 
+                    match += 1
+                else: 
+                    non += 1 
+
+        print("Matches:", match)
+        print("Non Matches:", non)
---
+++ b4_subtree.py
@@ -0,0 +1,152 @@
+#bloom4, each tree is a lsh 
+
+from bloom_filter2 import BloomFilter
+from treelib import Node, Tree
+import math, os, sys
+import eLSH as eLSH_import
+from LSH import LSH
+import pickle
+from Crypto.Util.Padding import pad, unpad
+
+import pyoram
+from pyoram.util.misc import MemorySize
+from pyoram.oblivious_storage.tree.path_oram import PathORAM
+
+from b4_node import node
+
+storage_name = "heap.bin"
+
+class subtree(object):
+    def __init__(self, branching_f, error_rate, lsh):
+        self.lsh = lsh
+        self.l = len(lsh)
+        self.branching_factor = branching_f
+        self.error_rate = error_rate
+        self.max_elem = None
+        
+        self.tree = None
+        self.depth = None 
+        self.root = branching_f-1
+
+    #calculate the number of max elements based on the size of the given list 
+    def calculate_max_elem(self, num_elements): 
+        #leaf nodes are hash output
+        # self.max_elem = self.l * (2**(math.ceil(math.log2(num_elements)))) #l * (2^{ceil(log_2(elements in list))})
+        self.max_elem = 2**(math.ceil(math.log2(num_elements)))
+
+    #calculate depth of the tree 
+    def calculate_depth(self):
+        self.depth = math.ceil(math.log(self.max_elem, self.branching_factor))
+
+    def calculate_LSH(self, item): 
+        res = [] 
+        for i, j in enumerate(self.lsh): 
+            res.append(j.hash(item))
+        return res
+
+    #creates a new node: bloom filter with elements from actual_elements
+    def new_node(self, current_node, parent_node, num_expected_elements=0, elements=None): 
+        if current_node == "root":
+            print("elements", elements)
+            #corner case: current_node == "root", parent_node == self.root, 
+            bf = BloomFilter(max_elements=(self.l), error_rate=self.error_rate)
+            _node_ = node(bf)
+            _node_.add_multiple(elements)
+            self.tree.create_node(current_node, self.root, data=_node_)
+        elif elements != None: 
+            bf = BloomFilter(max_elements=(self.l*num_expected_elements), error_rate=self.error_rate)
+            _node_ = node(bf)
+            _node_.add_multiple(elements)
+            self.tree.create_node(str(current_node), current_node, data=_node_, parent=parent_node)
+        else: 
+            self.tree.create_node(str(current_node), current_node, data=None, parent=parent_node)
+    
+    def build_tree(self, og_elements): 
+        self.tree = Tree() 
+        num_elements = len(og_elements)
+        level = 0 
+
+        self.calculate_max_elem(num_elements) #max number of elements WITH calculation of eLSH outputs 
+        self.calculate_depth()
+        elements = [self.calculate_LSH(i) for i in og_elements]
+
+        #initialize root node
+        self.new_node("root", self.root, num_elements, elements=elements)
+
+        current_node = self.root
+        parent_node = self.root-1 #-1 is just for it to work overall
+        
+        while level != self.depth: 
+            level += 1 
+            nodes_in_level = self.branching_factor**level
+            items_in_filter = self.branching_factor**(self.depth-level)
+            if level == self.depth:
+                for n in range(nodes_in_level):
+                    current_node += 1 
+                    if current_node % self.branching_factor == 0: 
+                        parent_node += 1 
+
+                        if n < self.l: 
+                            self.new_node(current_node, parent_node, num_expected_elements=1, elements=elements[n])
+                        else: 
+                            self.new_node(current_node, parent_node)
+
+            else: 
+                for n in range(nodes_in_level): 
+                    current_node += 1
+                    if current_node % self.branching_factor == 0: 
+                        parent_node += 1
+
+                    begin = n*items_in_filter
+                    end = (n*items_in_filter)+items_in_filter
+                    elements_in_filter = elements[begin:end]
+
+                    if elements_in_filter == []:
+                        self.new_node(current_node, parent_node)
+                    else: 
+                        self.new_node(current_node, parent_node, num_expected_elements=items_in_filter, elements=elements_in_filter)
+
+    def get_leaf_node(self, identifier): 
+        return self.tree.get_node(identifier)
+
+    def search(self, item):
+        depth = self.tree.depth()
+        stack = [] 
+        nodes_visited = [] 
+        leaf_nodes = [] 
+        returned_iris = []
+        access_depth = [[] for i in range(depth+1)] #nodes accessed per depth 
+        root_bf = self.tree[self.root].data
+
+        access_depth[0].append(self.root)
+        current_hash = self.calculate_LSH(item)
+        print("subtree search hash:", current_hash)
+        if root_bf.in_bloomfilter(current_hash): 
+            stack.append(self.root)
+
+        while stack != []: 
+            current_node = stack.pop()
+            nodes_visited.append(current_node)
+            children = self.tree.children(current_node) 
+            
+            if children != []: 
+                child_depth = self.tree.depth(current_node) + 1
+
+                for c in children: 
+                    child = c.identifier 
+                    access_depth[child_depth].append(child)                    
+                    if self.tree[child].data != None: 
+                        if self.tree[child].data.in_bloomfilter(current_hash): 
+                            stack.append(child)
+                            break
+            else: 
+                leaf_nodes.append(current_node)
+        
+        # for l in leaf_nodes: 
+        #     iris = self.hash_to_iris[self.tree.get_node(l).data]
+        #     returned_iris.append(iris)
+        # return nodes_visited, leaf_nodes, access_depth, returned_iris
+
+        print("subtree:", nodes_visited, "\n", leaf_nodes, "\n", access_depth)
+
+        return nodes_visited, leaf_nodes, access_depth
